5ec0bfdb8ce2e01ba2cd36b35f318fb32b199b50
Yuki Sato <yuki.fhs@gmail.com>
2022-09-19 19:02:28 +0900

    update

--- src/BasicStatistics/Variogram.jl
+++ src/BasicStatistics/Variogram.jl
@@ -2,16 +2,16 @@
     Variogram(y::AbstractArray, l::Int,; [data::AbstractArray=y, lag::Int=l]) -> Vector{Float64}
 
     # A brief description of variogram
-    This function calculate variogram statistics $(\tau)$:
+    This function calculate variogram statistics ``V(\\tau)``:
         ```math
-            V(\tau) := \left\langle \left(y(\tau) -y(0)\right)^2 \right\rangle.
+        \begin{align}
+            V(\\tau) := \left\langle \left(y(\tau) -y(0)\right)^2 \right\rangle.
+        \begin{align}
         ```
     Variogram statistic is often used to distinguish the class of statistical processes. In most cases, the variogram statistic grows power-law (``V(\tau)\tau^{2H}``), and that power-law exponent ``H`` can use for the classification of the dynamics:
-    \begin{itemize}
-        \item If ``H=0.5``, the dynamics might be Brownian motion.
-        \item If ``H>0.5``, the dynamics might be the long memory process.
-        \item If ``H<0.5``, the dynamics might be the short memory process.
-    \end{itemize}
+        - If ``H=0.5``, the dynamics might be Brownian motion.
+        - If ``H>0.5``, the dynamics might be the long memory process.
+        - If ``H<0.5``, the dynamics might be the short memory process.
 """
 """
     # Examples
